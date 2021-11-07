777f14f994744665c6c23001bc6ff860d541a32a
S179941 <s179941@student.pg.edu.pl>
2021-11-07 16:18:44 +0100

    (v0.3)Finished droplet description

--- src/operacjakrew/OperacjaKrew.java
+++ src/operacjakrew/OperacjaKrew.java
@@ -10,6 +10,22 @@ import java.util.*;
 public class OperacjaKrew {  
     public static void main(String[] args) {
         
+        Droplet kropelka = new Droplet();
+        Droplet kropelka2 = new Droplet();
+        
+        System.out.println("Pierwsza kropelka");
+        System.out.println(kropelka.get_xPosition());
+        System.out.println(kropelka.get_yPosition());
+        System.out.println(kropelka.get_bloodType());
+        kropelka.move_down();
+        System.out.println(kropelka.get_yPosition());
+        
+        System.out.println("Druga kropelka");
+        System.out.println(kropelka2.get_xPosition());
+        System.out.println(kropelka2.get_yPosition());
+        System.out.println(kropelka2.get_bloodType());
+        kropelka2.move_down();
+        System.out.println(kropelka2.get_yPosition());
     }
     
 }
